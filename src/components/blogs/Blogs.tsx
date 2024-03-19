0d8fff771a8900fb3a98b4a7cce92b205fdf3f2d
saadi925 <userid925925@gmail.com>
2024-03-19 17:40:48 +0500

    minor change

--- src/components/blogs/Blogs.tsx
+++ src/components/blogs/Blogs.tsx
@@ -1,4 +1,5 @@
 import AddIcon from "../../assets/AddIcon";
+import { redirect } from "../../keys";
 import MinHeading from "../MinHeading";
 import { SearchInput } from "../search/SearchInput";
 import BlogList from "./BlogList";
@@ -10,7 +11,7 @@ const Blogs = () => {
         <div className="flex items-center gap-4">
           <SearchInput isOpen />
           <a
-            href="http://post.dgspark.site"
+            href={redirect}
             className="bg-surface flex items-center border border-transparent transtion-all duration-300 text-white font-semibold rounded-full hover:bg-surface/60 active:bg-black active:border-primary py-1 px-5"
           >
             <AddIcon />
--- src/keys.ts
+++ src/keys.ts
@@ -1,1 +1,2 @@
 export const HOST = "https://dgspark.site/api";
+export const redirect = "https://dgspark.site/post";
